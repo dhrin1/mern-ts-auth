b10b877fbd428d36df0814ae78da5d1075728164
dhrin1 <alhdrin97@gmail.com>
2025-04-10 22:02:02 +0800

    041020251001

--- server/src/routes/auth.route.ts
+++ server/src/routes/auth.route.ts
@@ -1,10 +1,12 @@
-import { Router } from "express";
+import { Request, Response, Router } from "express";
 import {
   loginHandler,
   registerHandler,
   logoutHandler,
   refreshHandler,
   verifyEmailHandler,
+  sendPasswordResendHandler,
+  resetPasswordHandler,
 } from "../controllers/auth.controller";
 
 const authRoutes = Router();
@@ -14,5 +16,7 @@ authRoutes.post("/login", loginHandler);
 authRoutes.get("/refresh", refreshHandler);
 authRoutes.post("/logout", logoutHandler);
 authRoutes.get("/email/verify/:code", verifyEmailHandler);
+authRoutes.post("/password/forgot", sendPasswordResendHandler);
+authRoutes.post("/password/reset", resetPasswordHandler);
 
 export default authRoutes;
